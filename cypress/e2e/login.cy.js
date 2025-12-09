7ee62a6f01462259c8313fd5b4cf9e9bc132c290
Leandro Santos <santosleandro1805@gmail.com>
2025-12-09 07:20:03 -0300

    Realizar login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,12 @@
 describe('Login Test', () => {
 
     it('Realizar login', () =>{
-        
+        //arrange
+        cy.visit('https://www.saucedemo.com/')
+        //act
+        cy.get('[data-test="username"]').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        //assert
     })
 })
