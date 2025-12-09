67d194aec3ee86b3e926c5e42e1f7f0ae11b8992
Leandro Santos <santosleandro1805@gmail.com>
2025-12-09 07:50:57 -0300

    Login inválido

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -10,4 +10,21 @@ describe('Login Test', () => {
         //assert
         cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
     })
+
+    it.only('Login com Inválido', () =>{
+        //arrange
+        cy.visit('https://www.saucedemo.com/')
+        //act
+        cy.get('[data-test="username"]').type('usuario_invalido')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+        //assert
+        cy.get('[data-test="error"]')
+          .should(
+            'contain.text',
+             'Username and password do not match any user in this service'
+            )
+
+        cy.url().should('eq', 'https://www.saucedemo.com/')
+    })
 })
