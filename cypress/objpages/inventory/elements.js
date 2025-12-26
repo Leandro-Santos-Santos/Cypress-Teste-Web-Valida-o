6dcce32e9d176c906cd9078f4894bd0c9011fbde
Leandro Santos <santosleandro1805@gmail.com>
2025-12-26 09:13:08 -0300

    page object

--- cypress/objpages/inventory/elements.js
+++ cypress/objpages/inventory/elements.js
@@ -1,5 +1,8 @@
+import { remove } from "cypress/types/lodash"
+
 export const elements = {
     addToCart: (itemNome) => `[data-test="add-to-cart-${itemNome}"]`,
+    removeFromCart: (itemNome) => `[data-test="remove-${transform(itemNome)}"]`
 }
 
 function transform(texto) {
